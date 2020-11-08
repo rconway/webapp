a14e01c5a0ce810b39eb84202b1bf449037f9b6f
Richard Conway <richard@rconway.co.uk>
2020-11-08 21:03:45 +0000

    Use SpaHandler from goutils

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/rconway/webapp
 
 go 1.15
 
+replace github.com/rconway/goutils => ../goutils
+
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/markbates/pkger v0.17.1
+	github.com/rconway/goutils v0.0.0-00010101000000-000000000000
 )
--- main.go
+++ main.go
@@ -4,48 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/markbates/pkger"
+	"github.com/rconway/goutils/httputils"
 )
-
-// spaHandler implements the http.Handler interface, so we can use it
-// to respond to HTTP requests. The path to the static directory and
-// path to the index file within that static directory are used to
-// serve the SPA in the given static directory.
-type spaHandler struct {
-	staticPath string
-	indexPath  string
-}
-
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
-func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		// if we failed to get the absolute path respond with a 400 bad request
-		// and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// prepend the path with the path to the static directory
-	path = filepath.Join(h.staticPath, path)
-
-	// check whether a file exists at the given path
-	_, err = pkger.Stat(path)
-	if err != nil {
-		// If cannot stat file then use the root index file
-		r.URL.Path = filepath.Join(h.staticPath, h.indexPath)
-	}
-
-	// Use http.FileServer to serve the static dir
-	http.FileServer(pkger.Dir(h.staticPath)).ServeHTTP(w, r)
-}
 
 func main() {
 	log.Println("Starting webapp")
@@ -58,8 +21,9 @@ func main() {
 	})
 
 	// Static web site
-	_ = pkger.Dir("/my-app/build") // force pkger indexing of static files
-	spa := spaHandler{staticPath: "/my-app/build", indexPath: "index.html"}
+	pkger.Include("/my-app/build") // force pkger indexing of static files
+	spa := httputils.SpaHandler{Root: pkger.Dir("/my-app/build"), PathPrefix: "/my-app/build", IndexPath: "index.html", StatFunc: pkger.Stat}
+	// spa := httputils.SpaHandler{Root: http.Dir("my-app/build"), PathPrefix: "my-app/build", IndexPath: "index.html", StatFunc: os.Stat}
 	router.PathPrefix("/").Handler(spa)
 
 	// Start listening
