55914f9fd044d45c9b72d12b851c451ba9d05731
Richard Conway <richard@rconway.co.uk>
2020-11-11 21:41:58 +0000

    Add intelligent exe name for alpine build

--- build-alpine.sh
+++ build-alpine.sh
@@ -6,4 +6,6 @@ BIN_DIR="$(pwd)"
 
 trap "cd '${ORIG_DIR}'" EXIT
 
-docker run --rm -it -v $PWD/..:/src -e GOPATH_BUILD=/src/webapp/go-alpine golang:alpine sh /src/webapp/build.sh webapp-alpine
+EXENAME="${1:-$(basename "$PWD")-alpine}"
+
+docker run --rm -it -v $PWD/..:/src -e GOPATH_BUILD=/src/webapp/go-alpine golang:alpine sh /src/webapp/build.sh "${EXENAME}"
