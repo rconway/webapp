1f63222922c66a06394bd8072cf1c2cdea4d9dd9
Richard Conway <richard@rconway.co.uk>
2021-04-08 10:02:05 +0100

    Complete rework

--- go.mod
+++ go.mod
@@ -1,11 +1,11 @@
 module github.com/rconway/webapp
 
-go 1.15
+go 1.16
 
-replace github.com/rconway/goutils => ../goutils
+// replace github.com/rconway/goutils => ../goutils
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
-	github.com/markbates/pkger v0.17.1
-	github.com/rconway/goutils v0.0.0-00010101000000-000000000000
+	github.com/rconway/goutils v0.0.0-20210408085955-82b61323c806
 )
--- main.go
+++ main.go
@@ -1,33 +1,79 @@
 package main
 
 import (
+	"embed"
 	"fmt"
-	"log"
+	"io/fs"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/markbates/pkger"
 	"github.com/rconway/goutils/httputils"
 )
 
-func main() {
-	log.Println("Starting webapp")
+//go:embed app/build
+var appRoot embed.FS
+var wwwRoot, _ = fs.Sub(appRoot, "app/build")
 
+//================================================================================================================
+// Middlewares
+//================================================================================================================
+
+func loggingMiddleware(h http.Handler) http.Handler {
+	return handlers.CombinedLoggingHandler(os.Stdout, h)
+}
+
+//================================================================================================================
+// Simulated API endpoint
+//================================================================================================================
+
+func newApiRouter(prefix string, router *mux.Router) *mux.Router {
+	router.PathPrefix("/fred").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "zzz: api -> fred route\n")
+	})
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "zzz: api -> / route\n")
+	})
+	router.PathPrefix("").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "zzz: api -> BASE route\n")
+	})
+	return router
+}
+
+//================================================================================================================
+// Application (Single Page, e.g. Reactjs)
+//================================================================================================================
+
+func newAppRouter(prefix string, router *mux.Router) *mux.Router {
+	// Could 'Use' some more middlewares here, if needed
+
+	// Just a single route for the whole app
+	router.PathPrefix("").Handler(http.StripPrefix(prefix, httputils.NewSpaHandler(wwwRoot)))
+	return router
+}
+
+//================================================================================================================
+// Entrypoint
+//================================================================================================================
+
+func main() {
 	router := mux.NewRouter()
 
-	// API route
-	router.PathPrefix("/api").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "API path")
-	})
+	// Register middlewares
+	router.Use(loggingMiddleware)
 
-	// Static web site
-	pkger.Include("/my-app/build") // force pkger indexing of static files
-	spa := httputils.SpaHandler{Root: pkger.Dir("/my-app/build"), PathPrefix: "/my-app/build", IndexPath: "index.html", StatFunc: pkger.Stat}
-	// spa := httputils.SpaHandler{Root: http.Dir("my-app/build"), PathPrefix: "my-app/build", IndexPath: "index.html", StatFunc: os.Stat}
-	router.PathPrefix("/").Handler(spa)
+	// API
+	newApiRouter("/api", router.PathPrefix("/api").Subrouter())
 
-	// Start listening
-	port := 8080
-	log.Println("Listening on port:", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), router))
+	// Application (SPA)
+	{
+		// This is at the root URL path since Reactjs doesn't seem to accommodate a path
+		// prefix very easily.
+		prefix := ""
+		appSubRouter := router.PathPrefix("").Subrouter()
+		newAppRouter(prefix, appSubRouter)
+	}
+
+	http.ListenAndServe(":8080", router)
 }
