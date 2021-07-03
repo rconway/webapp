de55c881dc7e8ef78543785bae8e1346243cd38d
Richard Conway <richard@rconway.co.uk>
2021-07-03 07:44:11 +0100

    Update login handler

--- api/login.go
+++ api/login.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
 )
@@ -11,7 +12,16 @@ import (
 //================================================================================================================
 
 func apiLoginHandler(router *mux.Router) {
-	router.PathPrefix("").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		viewTemplates.ExecuteTemplate(w, "login.html", nil)
+	// exact path '/'
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		viewTemplates.ExecuteTemplate(w, "login.html", "github/initiate")
 	})
+
+	// exact path <blank>
+	router.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, path.Base(r.URL.Path)+"/", http.StatusPermanentRedirect)
+	})
+
+	// unmatched route - NOT FOUND
+	router.PathPrefix("").Handler(http.NotFoundHandler())
 }
