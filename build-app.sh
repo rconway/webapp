3b894b0cca6a617c143387c8a48a913dbe98ad9e
Richard Conway <richard@rconway.co.uk>
2022-07-07 08:32:30 +0100

    Fix-up build-app.sh

--- build-app.sh
+++ build-app.sh
@@ -8,6 +8,10 @@ trap "cd '${ORIG_DIR}'" EXIT
 
 pushd app
 echo "Building the React app..."
+if [ ! -d node_modules ]; then
+  echo "  Installing dependencies..."
+  npm install
+fi
 npm run build
 echo "...built app."
 popd
